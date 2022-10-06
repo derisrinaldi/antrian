33455c96816deed3ca4fe098889f7cea584edf97
derisrinaldi0 <derisrinaldi0@gmail.com>
2022-10-06 13:02:39 +0700

    start coding

---
+++ resources/views/layouts/admin/app.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Antrian</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+
+    @stack('style')
+</head>
+
+<body>
+
+    @include('layouts.component.admin-header')
+
+    <div class="container-fluid">
+        <div class="row">
+
+            @include('layouts.component.admin-sidebar')
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('container')
+            </main>
+        </div>
+    </div>
+
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+
+    @stack('script')
+</body>
+
+</html>
